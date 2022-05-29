df6d6d881dda977fd15cdf339d5dd98f436e8645
Minhtet Htoon <waimarlwin35@gmail.com>
2022-05-28 22:26:54 -0600

    Fix some stuff

--- components/Header.tsx
+++ components/Header.tsx
@@ -56,10 +56,9 @@ export default function SiteHeader() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="/">
-              <span className="sr-only">Workflow</span>
+            <Link href="/">
               <Image src="/globe_notext.png" alt="team logo" height={48} width={48}/>
-            </a>
+            </Link>
           </div>
           <div className="-mr-2 -my-2 md:hidden">
             <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-teal-400">
@@ -120,7 +119,7 @@ export default function SiteHeader() {
               )}
             </Popover>
             <Link href="/calendar"><a className="text-base font-medium text-gray-500 hover:text-gray-900">Calendar</a></Link>
-            <Link href="https://www.firstinspires.org/robotics/frc" target="_blank" className="text-base font-medium text-gray-500 hover:text-gray-900">FIRST Program Info</Link>
+            <Link href="https://www.firstinspires.org/robotics/frc" className="text-base font-medium text-gray-500 hover:text-gray-900">FIRST Program Info</Link>
             <Popover className="relative">
               {({ open }) => (
                 <>
