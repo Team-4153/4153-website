2a343a3baeaf437239d213505e65fe80c4a77f34
minhtet-htoon <waimarlwin35@gmail.com>
2022-06-28 18:44:22 -0600

    Start populating assets

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -2,36 +2,6 @@ import { AnnotationIcon, GlobeAltIcon, LightningBoltIcon, ScaleIcon } from '@her
 import Image from "next/image";
 import React from "react";
 
-const features = [
-  {
-    name: 'Company 1',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: GlobeAltIcon,
-    logo:"https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png?w=640"
-  },
-  {
-    name: 'Company 2',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: ScaleIcon,
-    logo:"https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png?w=640"
-  },
-  {
-    name: 'Org 1',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: LightningBoltIcon,
-    logo:"https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png?w=640"
-  },
-  {
-    name: 'Org 2',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: AnnotationIcon,
-    logo:"https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png?w=640"
-  },
-]
 
 export default function Sponsors() {
   return (
@@ -48,19 +18,37 @@ export default function Sponsors() {
       </div>
       <div className="w-full bottom-2 flex flex-col gap-4 justify-center items-center py-6">
         <div className="flex h-60 gap-4 w-[80%] flex-row">
-          <img className="bg-blue-400 rounded-2xl basis-1/4 h-full object-cover" src="/placeholder.png"/>
-          <img className="bg-blue-400 rounded-2xl basis-1/3 h-full object-cover" src="/placeholder.png"/>
-          <img className="bg-blue-400 rounded-2xl basis-6/12 h-full object-cover" src="/placeholder.png"/>
+          <div className="bg-blue-400 rounded-2xl basis-1/4 h-full object-cover justify-items-center align-middle flex">
+            <img className='align-middle items-center object-contain' src="/N3B.png"/>
+          </div>
+          <div className="bg-blue-400 rounded-2xl basis-1/3 h-full object-cover justify-items-center align-middle flex">
+            <img className='align-middle items-center object-contain' src="/merrick.png"/>
+          </div>
+          <div className="bg-blue-400 rounded-2xl basis-6/12 h-full justify-items-center align-middle flex">
+            <img className='align-middle items-center object-contain' src="/EBT.png"/>
+          </div>
         </div>
         <div className="flex h-60 gap-4 w-[80%] flex-row-reverse">
-          <img className="bg-blue-400 rounded-2xl basis-1/4 h-full object-cover" src="/placeholder.png"/>
-          <img className="bg-blue-400 rounded-2xl basis-1/3 h-full object-cover" src="/placeholder.png"/>
-          <img className="bg-blue-400 rounded-2xl basis-6/12 h-full object-cover" src="/placeholder.png"/>
+          <div className="bg-blue-400 rounded-2xl basis-1/4 h-full object-cover justify-items-center align-middle flex">
+            <img className='align-middle items-center object-contain' src="/laps.png"/>
+          </div>
+          <div className="bg-blue-400 rounded-2xl basis-1/3 h-full object-cover justify-items-center align-middle flex">
+            <img className='align-middle items-center object-contain' src="/asme.png"/>
+          </div>
+          <div className="bg-blue-400 rounded-2xl basis-6/12 h-full justify-items-center align-middle flex">
+            <img className='align-middle items-center object-contain' src="/CENT.png"/>
+          </div>
         </div>
         <div className="flex h-60 gap-4 w-[80%] flex-row-reverse">
-          <img className="bg-blue-400 rounded-2xl basis-[44.444444444%] h-full object-cover" src="/placeholder.png"/>
-          <img className="bg-blue-400 rounded-2xl basis-1/3 h-full object-cover" src="/placeholder.png"/>
-          <img className="bg-blue-400 rounded-2xl basis-[22.222222222%] h-full object-cover" src="/placeholder.png"/>
+        <div className="bg-blue-400 rounded-2xl basis-[44.44444444%] h-full object-cover justify-items-center align-middle flex">
+            <img className='align-middle items-center object-contain' src="/DSS.png"/>
+          </div>
+          <div className="bg-blue-400 rounded-2xl basis-1/3 h-full object-cover justify-items-center align-middle flex">
+            <img className='align-middle items-center object-contain' src="/HPM.png"/>
+          </div>
+          <div className="bg-blue-400 rounded-2xl basis-6/12 h-full justify-center align-middle flex">
+            <img className='align-middle items-center object-contain' src="/LAPSF.png"/>
+          </div>
         </div>
       </div>
     </div>
