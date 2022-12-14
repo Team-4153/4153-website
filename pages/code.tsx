b79c82bd1141973d6edb2b0950d0ae83da197726
Minhtet Htoon <waimarlwin35@gmail.com>
2022-12-13 18:29:54 -0700

    Finalize code.tsx

--- pages/code.tsx
+++ pages/code.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { BrandGithub } from "tabler-icons-react";
+import Image from "next/image";
 
 
 export default function code() {
@@ -14,60 +15,68 @@ export default function code() {
                 <div className="flex py-6 flex-row mx-60">
                 <div className="basis-2/3 flex flex-col relative px-6 border-r-2">
                         <p>
-                            This code is the core of our 2022 competition robot. It uses a MKII swerve drive and the code is availaible to all teams on GitHub
+                            This code is the core of our 2022 competition robot. It uses a MKII swerve drive and the code is available to all teams on GitHub
                         </p>
-                        <br></br>
+                        <div className="h-20"/>
                         <div className="w-full h-fit flex justify-center items-center">
-                                <button className="relative flex w-fit items-center justify-center object-bottom px-4 py-2 font-semibold text-sm bg-primary text-white rounded-full shadow-sm">
+                                <button className="hover:scale-110 transition duration-0 hover:duration-150 relative flex w-fit items-center justify-center object-bottom px-4 py-2 font-semibold text-sm bg-gradient-to-r from-cyan-500 to-blue-500 text-white rounded-full shadow-sm">
                                     <BrandGithub className="float-left h-full"/>
                                     <div className="w-2"></div>
                                     <Link href={"https://github.com/Team-4153/Basic-Swerve"}>
-                                        <a target="_blank" className="hover:text-primary-600 inline underline">Access GitHub Repository</a>
+                                        <a target="_blank" className="hover:text-gray-300 inline underline">Access GitHub Repository</a>
                                     </Link> 
                                 </button>
                         </div>
                         
                     </div>
                     <div className="basis-1/3 px-6 ">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/tz5aEal91OM" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                      <Image src="/placeholder.png" height="325" width="560" alt="placeholder.png"/>
                     </div>
                 </div>
                 <p className="text-black uppercase border-t-2 border-b-2 text-primary-600 font-light py-6 text-3xl ">
-                    Drill Press
+                    Autonomous
                 </p>
                 <div className="flex py-6 flex-row mx-60">
                     <div className="basis-1/3 px-6 border-r-2">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/skLueD5ZWAg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <Image src="/placeholder.png" height="325" width="560" alt="placeholder.png"/>
                     </div>
                     <div className="basis-2/3 px-6">
                         <p>
-                            The drill press is a machine used for drilling holes vertically in a controlled manner. This training is recommended for all team members who will be working in the shop and on the robot throughout the year.
+                            This is the base code that we use to build autonomous routines on top of. It really isn&apos;t for any game in particular but simply a starting point
                         </p>
-                        <p className="py-6 text-2xl font-light text-primary-500 uppercase">
-                            Safety Forms
-                        </p>
-                        <p>
-                            <Link href={"/drill-press.pdf"}><a target="_blank" className="hover:text-primary inline underline">Download Safety Form</a></Link>
-                        </p>
+                      <div className="h-20"/>
+                      <div className="w-full h-fit flex justify-center items-center">
+                        <button className="hover:scale-110 transition duration-0 hover:duration-150 relative flex w-fit items-center justify-center object-bottom px-4 py-2 font-semibold text-sm bg-gradient-to-r from-cyan-500 to-blue-500 text-white rounded-full shadow-sm">
+                          <BrandGithub className="float-left h-full"/>
+                          <div className="w-2"></div>
+                          <Link href={"https://github.com/Team-4153/Basic-Swerve"}>
+                            <a target="_blank" className="hover:text-gray-300 inline underline">Access GitHub Repository</a>
+                          </Link>
+                        </button>
+                      </div>
                     </div>
                 </div>
                 <p className="text-black uppercase border-b-2 text-primary-600 border-t-2 font-light py-6 text-3xl ">
-                    Band Saw
+                    Vision
                 </p>
                 <div className="flex py-6 flex-row mx-60">
                 <div className="basis-2/3 px-6 border-r-2">
-                        <p>
-                            Bandsaws are machines used for cutting stock down to size. There are 2 types of bandsaws, the vertical and the falling bandsaw. This video and document covers both.
-                        </p>
-                        <p className="py-6 text-2xl font-light text-primary-500 uppercase">
-                            Safety Forms
-                        </p>
-                        <p>
-                            <Link href={"/bandsaw.pdf"}><a target="_blank" className="hover:text-primary inline underline">Download Safety Form</a></Link>
-                        </p>
+                  <p>
+                    This is the base code that we use to give our robots vision capabilities
+                  </p>
+                  <div className="h-20"/>
+                  <div className="w-full h-fit flex justify-center items-center">
+                    <button className="hover:scale-110 transition duration-0 hover:duration-150 relative flex w-fit items-center justify-center object-bottom px-4 py-2 font-semibold text-sm bg-gradient-to-r from-cyan-500 to-blue-500 text-white rounded-full shadow-sm">
+                      <BrandGithub className="float-left h-full"/>
+                      <div className="w-2"></div>
+                      <Link href={"https://github.com/Team-4153/Basic-Swerve"}>
+                        <a target="_blank" className="hover:text-gray-300 inline underline">Access GitHub Repository</a>
+                      </Link>
+                    </button>
+                  </div>
                     </div>
                     <div className="basis-1/3 px-6 ">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/BX8sc3GO6Uw" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                      <Image src="/placeholder.png" height="325" width="560" alt="placeholder.png"/>
                     </div>
                 </div>
                 </div>
